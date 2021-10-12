29098fbb3becac3ac0cd7abed59298adcf9c19fb
Bogdan Konovalov <bodikkonovalov@gmail.com>
2021-10-12 19:57:16 +0300

    About plus some fixes

---
+++ laravel/resources/views/layouts/blocks/about.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div class="about-us">
+        <div class="side-text">
+            <p>about</p>
+          <svg width="1" height="80" viewBox="0 0 1 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line x1="0.500122" y1="2.18557e-08" x2="0.500119" y2="80" stroke="black"/>
+              </svg>
+        </div>
+        <div class="about-text">
+        <h2>The team</h2>
+        <p>What’s better than one 
+            amazing photographer? 
+            A team of friendly professionals.
+            The entire team is professional, fun, organized, and the pictures are always incredible. We make each other better, every single day.</p>
+        </div>
+        <div class="about-photo-block">
+            <div class="left-block">
+                <div class="inside-photo">
+                    <img src="/img/left-photo.png" alt="">
+                </div>
+            </div>
+            <div class="right-block">
+                <div class="inside-photo">
+                    <img src="/img/right-photo.png" alt="">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
